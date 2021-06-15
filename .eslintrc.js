aef81349489dc436b1164bc4478f956dc702c820
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-16 05:53:11 +0800

    update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-// Copyright 2017-2021 @polkadot/api authors & contributors
+// Copyright 2018-2021 @setheum-js/api authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-
-const base = require('@polkadot/dev/config/eslint.cjs');
 
 module.exports = {
   ...base,
@@ -24,7 +22,5 @@ module.exports = {
     ...base.rules,
     // add override for any (a metric ton of them, initial conversion)
     '@typescript-eslint/no-explicit-any': 'off',
-    // this seems very broken atm, false positives
-    '@typescript-eslint/unbound-method': 'off'
   }
 };
