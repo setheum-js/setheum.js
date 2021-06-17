d046cdb0b4bafeda7e0baf017eed49fdb7e5b806
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-17 22:39:15 +0800

    update wallet sdk

---
+++ packages/sdk-wallet/src/wallet-base.ts
@@ -0,0 +1,55 @@
+import { Token, TokenBalance, MaybeAccount, MaybeCurrency, ObOrPromiseResult } from '@setheum-js/sdk-core';
+
+import { ApiRx, ApiPromise } from '@polkadot/api';
+import { PriceData, PriceDataWithTimestamp } from './types';
+
+export abstract class WalletBase<T extends ApiRx | ApiPromise> {
+  protected api: T;
+
+  protected constructor(api: T) {
+    this.api = api;
+  }
+
+  /**
+   * @name getAllTokens
+   * @description get all available currencies
+   */
+  abstract getAllTokens(): Token[];
+
+  /**
+   * @name getToken
+   * @description get the currency
+   */
+  abstract getToken(currency: MaybeCurrency): Token;
+
+  // get balance
+
+  /**
+   * @name getBalance
+   * @description get the balance of the currency
+   */
+  abstract queryBalance(account: MaybeAccount, currency: MaybeCurrency): ObOrPromiseResult<T, TokenBalance>;
+
+  /**
+   * @name queryPrices
+   * @description get prices of tokens
+   */
+  abstract queryPrices(tokens: MaybeCurrency[]): ObOrPromiseResult<T, PriceData[]>;
+
+  /**
+   * @name queryPrice
+   * @description get the price
+   */
+  abstract queryPrice(currency: MaybeCurrency): ObOrPromiseResult<T, PriceData>;
+
+  /**
+   * @name queryOraclePrice
+   * @description get the oracle feed price
+   */
+  abstract queryOraclePrice(): ObOrPromiseResult<T, PriceDataWithTimestamp[]>;
+
+  /**
+   * @name subscribeOracleFeeds
+   */
+  abstract subscribeOracleFeed(provider: string): ObOrPromiseResult<T, PriceDataWithTimestamp[]>;
+}
