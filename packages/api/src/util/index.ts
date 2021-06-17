75d300d98033c1f64f88b448aae0734c1b455bd5
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-17 21:53:59 +0800

    updating setheum.js

--- packages/api/src/util/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2021 @polkadot/api authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export * from './decorate';
-
-export { filterEvents } from './filterEvents';
-export { isKeyringPair } from './isKeyringPair';
-export { l } from './logging';
