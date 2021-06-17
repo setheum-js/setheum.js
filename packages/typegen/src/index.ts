17e5244afcde906c336ccc65b045381a8c59af98
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-17 22:01:32 +0800

    upd in

--- packages/typegen/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2017-2021 @polkadot/typegen authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import './detectPackage';
-
-export { packageInfo } from './packageInfo';
-export { formatType } from './util/formatting';
