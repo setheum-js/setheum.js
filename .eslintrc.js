aa37f1ba81f3d4fda700519b23c886da043067e3
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-17 21:50:30 +0800

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021 @setheum-js/api authors & contributors
+// Copyright 2018-2021 @setheum-js/setheum.js authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
