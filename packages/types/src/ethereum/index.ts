3d48dad49db1273ef30ce84cb424c2f0815a8bfa
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-17 22:09:08 +0800

    update

--- packages/types/src/ethereum/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2017-2021 @polkadot/types authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export { GenericEthereumAccountId } from './AccountId';
-export { GenericEthereumLookupSource } from './LookupSource';
