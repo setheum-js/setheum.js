cacd93006a6b3d401594961cf9978685586af0fb
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-17 22:25:32 +0800

    update index

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,63 @@
-// Copyright 2017-2021 @polkadot/api authors & contributors
-// SPDX-License-Identifier: Apache-2.0
+import { derive as ormlDerives } from '@open-web3/orml-api-derive';
+import { derive as setheumDerives } from '@setheum-js/setheum.js-derive';
+import {
+  rpc as setheumRpc,
+  types as setheumTypes,
+  typesAlias as setheumTypesAlias,
+  typesBundle as setheumTypesBundle,
+  signedExtensions as setheumSignedExtensions
+} from '@setheum-js/types';
+import { ApiOptions } from '@polkadot/api/types';
 
-import './detectPackage';
+export const defaultOptions: ApiOptions = {
+  types: setheumTypes,
+  rpc: setheumRpc
+};
 
-export { Keyring } from '@polkadot/keyring';
-export { WsProvider } from '@polkadot/rpc-provider/ws';
-
-export { packageInfo } from './packageInfo';
-export { ApiPromise } from './promise';
-export { SubmittableResult } from './submittable';
-export { ApiRx } from './rx';
+export const options = ({
+  types = {},
+  rpc = {},
+  typesAlias = {},
+  typesBundle = {},
+  signedExtensions,
+  ...otherOptions
+}: ApiOptions = {}): ApiOptions => ({
+  types: {
+    ...types
+  },
+  rpc: {
+    ...setheumRpc,
+    ...rpc
+  },
+  typesAlias: {
+    ...setheumTypesAlias,
+    ...typesAlias
+  },
+  derives: {
+    ...ormlDerives,
+    ...setheumDerives
+  },
+  typesBundle: {
+    ...typesBundle,
+    spec: {
+      ...typesBundle.spec,
+      setheum: {
+        ...setheumTypesBundle?.spec?.setheum,
+        ...typesBundle?.spec?.setheum
+      },
+      newrome: {
+        ...setheumTypesBundle?.spec?.newrome,
+        ...typesBundle?.spec?.newrome
+      },
+      neom: {
+        ...setheumTypesBundle?.spec?.neom,
+        ...typesBundle?.spec?.newrome
+      }
+    }
+  },
+  signedExtensions: {
+    ...setheumSignedExtensions,
+    ...signedExtensions
+  },
+  ...otherOptions
+});
