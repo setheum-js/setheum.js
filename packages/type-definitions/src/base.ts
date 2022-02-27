b9f885a47c8fa27acecfc2c1f881681258724961
Muhammad-Jibril B.A <jbashir52@gmail.com>
2022-02-28 02:25:16 +0800

    remove setcloud and ipfs related modules
    remove setcloud and ipfs related modules

--- packages/type-definitions/src/base.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  rpc: {},
-  types: {
-    AccountInfo: 'AccountInfoWithProviders',
-    Address: 'AccountId',
-    AddressInfo: 'Vec<u8>',
-    LookupSource: 'AccountId',
-  },
-};
